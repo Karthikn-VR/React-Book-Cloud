68917a43d48a00b5fd58782cc2c7be4715e5d73a
Karthikn-VR <karthikeyan33607@gmail.com>
2025-08-03 17:40:19 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  allowedHosts: ['react-book-cloud-1.onrender.com'],
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
