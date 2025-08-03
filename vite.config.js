b03290536ab447d4d8fe072adbfdacd4b43481b3
Karthikn-VR <karthikeyan33607@gmail.com>
2025-08-03 17:45:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  allowedHosts: ['react-book-cloud-1.onrender.com'],
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
+    allowedHosts: [
+      'react-book-cloud-1.onrender.com',
+      'react-book-cloud-2.onrender.com'
+    ]
   }
 })
